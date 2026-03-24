3ce566d88e9dc85df80d70f19076b9d4e4f91666
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-24 12:36:42 +0530

    chore: update modules/nixos/system/oomd.nix

--- modules/nixos/system/oomd.nix
+++ modules/nixos/system/oomd.nix
@@ -12,19 +12,5 @@ in {
     };
 
     services.nix-daemon.serviceConfig.OOMScoreAdjust = mkDefault 350;
-
-    tmpfiles.settings."10-oomd-root".w = {
-      # Enables storing of the kernel log (including stack trace) into pstore upon a panic or crash.
-      "/sys/module/kernel/parameters/crash_kexec_post_notifiers" = {
-        age = "-";
-        argument = "Y";
-      };
-
-      # Enables storing of the kernel log upon a normal shutdown (shutdown, reboot, halt).
-      "/sys/module/printk/parameters/always_kmsg_dump" = {
-        age = "-";
-        argument = "N";
-      };
-    };
   };
 }
