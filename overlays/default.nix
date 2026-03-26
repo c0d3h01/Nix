443b9fac624797f7678ee5d1d5ec5c3c782a9748
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-26 19:08:55 +0530

    refactor(overlays): remove nixgl overlay and associated wrapper for cleaner configuration

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,4 @@
 # exposed as flake.outputs.overlays
 {inputs}: {
-  nixgl = inputs.nixgl.overlays.default;
   nur = inputs.nur.overlays.default;
-  nixglWrapper = import ./nixgl-wrapper.nix {isNixOS = false;};
 }
