19536813712ac0c4c9f2d27f8a04f617d607a1ab
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-27 08:33:39 +0530

    fix(user.nix): update locale settings to remove UTF-8 suffix

--- modules/nixos/system/user.nix
+++ modules/nixos/system/user.nix
@@ -8,7 +8,7 @@
   time.timeZone = "Asia/Kolkata";
 
   # Locale (UTF-8)
-  i18n.defaultLocale = "en_IN.UTF-8";
+  i18n.defaultLocale = "en_IN";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_IN";
     LC_IDENTIFICATION = "en_IN";
@@ -19,7 +19,7 @@
     LC_PAPER = "en_IN";
     LC_TELEPHONE = "en_IN";
     LC_TIME = "en_IN";
-    LC_ALL = "en_IN.UTF-8";
+    LC_ALL = "en_IN";
   };
 
   # System State Version
