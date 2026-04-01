472fee912810a66936e6d2259af03e976b692d0e
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-04-01 17:22:12 +0530

    chore(nixvim): disable treesitter folding
    - stop enabling the treesitter folding integration
    - keep syntax highlighting and indent support unchanged

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -29,7 +29,7 @@
         enable = true;
         highlight.enable = true;
         indent.enable = true;
-        folding.enable = true;
+
         grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
           bash
           c
