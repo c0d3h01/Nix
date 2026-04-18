670ede6a2f72e132cbe41d94354e679e38276950
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-04-18 14:28:09 +0000

    feat: add more users to nixos host

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -11,11 +11,20 @@ in {
   ];
 
   users.users = {
-    root.openssh.authorizedKeys.keys = sshKeys;
+    root = {
+      hashedPassword = "$y$j9T$KAIygNaHsr4EgJcn2Jto.1$9gK08qEPSw/Fll//2TCe5ijYIqavtnvoundIux8Uy5/";
+      openssh.authorizedKeys.keys = sshKeys;
+    };
+
+    c0d3h01 = {
+      home = "/home/c0d3h01";
+      hashedPassword = "$y$j9T$jbMpDi1jashn36Vczb8jO/$E8M0edjvWOZg24Su5bFWaQ5tHcPkwyQ8HdzkAMx0km7";
+      openssh.authorizedKeys.keys = sshKeys;
+    };
 
     anon = {
       home = "/home/anon";
-      hashedPassword = "$y$j9T$jbMpDi1jashn36Vczb8jO/$E8M0edjvWOZg24Su5bFWaQ5tHcPkwyQ8HdzkAMx0km7";
+      hashedPassword = "$y$j9T$IKXrH64o2Ni7mqraKV6ke/$3FtfHxmcWPIKaziQ40uzjdyMeFBZsDRWGmAeq9KbBb2";
       openssh.authorizedKeys.keys = sshKeys;
     };
   };
