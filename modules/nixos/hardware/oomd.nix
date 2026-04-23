efd7fda2d04d9087489f039c89d04e48e179cc99
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-04-23 11:08:58 +0530

    chore: oomd - droped useless comments

--- modules/nixos/hardware/oomd.nix
+++ modules/nixos/hardware/oomd.nix
@@ -6,11 +6,9 @@
       enableUserSlices = true;
       enableSystemSlice = true;
 
-      # Hardcoded for 6GB RAM: React faster (3s) at lower pressure (60%)
       settings.OOM = {
         DefaultMemoryPressureLimit = "60%";
         DefaultMemoryPressureDurationSec = "3s";
-        # Since we have ample ZRAM, we can be less aggressive on swap limit
         DefaultSwapUsedLimit = "90%";
         PreferredMemoryPressureLimit = "95%";
       };
