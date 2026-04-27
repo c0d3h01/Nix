2ad2f9d789ea44eaba5c3f23d460f13c5b03da0c
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-04-27 01:22:19 +0000

    feat: minimal secure git+gpg setup for gnome
    - Enable GPG with strong crypto defaults and GNOME pinentry
    - Configure git for signed commits/tags and GNOME keyring credential storage
    - Set up gpg-agent with sensible cache and GNOME integration
    - Ensure data integrity with fsync and secure defaults
    - Keep configuration minimal and focused on security

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,78 +1,53 @@
 {pkgs, ...}: {
   programs.gpg = {
     enable = true;
-
     settings = {
       keyid-format = "0xlong";
       with-fingerprint = true;
-      with-keygrip = true;
-      fixed-list-mode = true;
-      no-comments = true;
-      no-emit-version = true;
       charset = "utf-8";
-
-      personal-digest-preferences = "SHA512 SHA384 SHA256";
-      personal-cipher-preferences = "AES256 AES192 AES";
-      default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+      personal-digest-preferences = "SHA512";
       cert-digest-algo = "SHA512";
       s2k-digest-algo = "SHA512";
       s2k-cipher-algo = "AES256";
       s2k-count = "65011712";
-
-      require-cross-certification = true;
-      verify-options = "show-uid-validity";
-      list-options = "show-uid-validity";
+      no-comments = true;
+      no-emit-version = true;
     };
   };
 
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
-    pinentry.package = pkgs.pinentry-all;
-
-    defaultCacheTtl = 28800;
-    maxCacheTtl = 86400;
-    defaultCacheTtlSsh = 28800;
-    maxCacheTtlSsh = 86400;
-
-    extraConfig = ''
-      allow-loopback-pinentry
-    '';
+    pinentry.package = pkgs.pinentry-gnome3;
+    defaultCacheTtl = 14400;
+    maxCacheTtl = 28800;
+    extraConfig = "allow-loopback-pinentry";
   };
 
   programs.git = {
     enable = true;
-    lfs.enable = true;
-
     signing = {
       key = "A7A7A1725FBF10AB04BF1355B4242C21BAF74B7C";
       signByDefault = true;
     };
-
     settings = {
       user = {
         name = "Harshal Sawant";
         email = "harshalsawant.dev@gmail.com";
       };
-
       gpg.program = "${pkgs.gnupg}/bin/gpg";
       tag.gpgSign = true;
-
       init.defaultBranch = "main";
       pull.rebase = true;
-      push.autoSetupRemote = true;
-      rebase.autoStash = true;
-      merge.conflictStyle = "zdiff3";
-      diff.algorithm = "histogram";
-      rerere.enabled = true;
       core = {
-        autocrlf = "input";
-        whitespace = "trailing-space,space-before-tab";
         fsync = "committed";
+        sshCommand = "ssh -T";
       };
-
-      credential.helper = "store --file ~/.git-credentials";
-      url."git@github.com:".insteadOf = "https://github.com/";
+      credential.helper = "libsecret";
     };
+  };
+
+  environment.sessionVariables = {
+    GPG_TTY = "$TTY";
   };
 }
