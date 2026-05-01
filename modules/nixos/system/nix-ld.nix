4c06ae5d8657e0afe766bf663ab87899cddd8177
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-05-01 21:28:49 +0530

    refactored(nix-ld): include variuos fhs compact fuse system packages thanks to @mic92

--- modules/nixos/system/nix-ld.nix
+++ modules/nixos/system/nix-ld.nix
@@ -1,13 +1,65 @@
-{pkgs, ...}: {
-  programs.nix-ld = {
-    enable = true;
-    libraries = with pkgs; [
-      stdenv.cc.cc.lib
+{pkgs, lib, config, ...}: let
+  inherit (lib) mkDefault optionals;
+in {
+  programs.nix-ld.enable = mkDefault true;
+  programs.nix-ld.libraries =
+    with pkgs;
+    [
+      acl
+      attr
+      bzip2
+      dbus
+      expat
+      fontconfig
+      freetype
+      fuse3
+      icu
+      libnotify
+      libsodium
+      libssh
+      libunwind
+      libusb1
+      libuuid
+      nspr
+      nss
+      stdenv.cc.cc
+      util-linux
       zlib
       zstd
-      openssl
-      curl
-      libuuid
+    ]
+    ++ optionals (config.hardware.graphics.enable) [
+      pipewire
+      cups
+      libxkbcommon
+      pango
+      mesa
+      libdrm
+      libglvnd
+      libpulseaudio
+      atk
+      cairo
+      alsa-lib
+      at-spi2-atk
+      at-spi2-core
+      gdk-pixbuf
+      glib
+      gtk3
+      libGL
+      libappindicator-gtk3
+      vulkan-loader
+      libx11
+      libxscrnsaver
+      libxcomposite
+      libxcursor
+      libxdamage
+      libxext
+      libxfixes
+      libxi
+      libxrandr
+      libxrender
+      libxtst
+      libxcb
+      libxkbfile
+      libxshmfence
     ];
-  };
 }
